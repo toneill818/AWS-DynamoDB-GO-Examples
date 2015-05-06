8355fc626f727f27b80f540fea4de5ecc37b4fd4
Thomas O'Neill <tmo21@zips.uakron.edu>
2015-05-06 15:34:56 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # AWS-DynamoDB-GO-Examples
+
+This repository holds code example showing the basics of using dynamodb with GO.
+Inorder for this code to work you will need to have your aws credentials saved in the .aws folder or in your environment variables. 
---
+++ put_item.go
@@ -0,0 +1,65 @@
+/*
+	The code below is an example on how to put data into amazon's dynamodb
+	using GO. This is the minimum requirements to get data into a table.
+	For more information about how to use the package visit this site
+	https://github.com/awslabs/aws-sdk-go/blob/master/service/dynamodb/examples_test.go
+	Note you need to have your AWS access key and secret access key saved in your home
+	directory in a file called credentials inside of the .aws folder. 
+*/
+
+package main
+import(
+	"fmt"
+	
+	"github.com/awslabs/aws-sdk-go/aws"
+	"github.com/awslabs/aws-sdk-go/aws/awsutil"
+	"github.com/awslabs/aws-sdk-go/service/dynamodb"
+)
+/*
+	Below is an example on how to insert items into a dynamodb table.
+	Note: the key "id" is a primary key on the example table.
+*/
+func main(){
+	svc := dynamodb.New(&aws.Config{Region: "us-east-1"})
+
+	params := &dynamodb.PutItemInput{
+		Item: &map[string]*dynamodb.AttributeValue{
+			// This is our primary key for the item being inserted
+			"id": &dynamodb.AttributeValue{
+				S: aws.String("Primary Key"),
+			},
+			// key1 maps to a number
+			"key1": &dynamodb.AttributeValue{
+				N: aws.String("12345"),
+			},
+			// Holds a boolean value
+			"boolean": &dynamodb.AttributeValue{
+				BOOL: aws.Boolean(false),
+			},
+			// Holds an array of strings
+			"string array": &dynamodb.AttributeValue{
+				SS: []*string{
+					aws.String("element 1"),
+					aws.String("element 2"),
+					aws.String("element 3"),
+				},
+			},
+			"Bytes": &dynamodb.AttributeValue{
+				B: []byte("message"),
+			},
+			"NULL?": &dynamodb.AttributeValue{
+				NULL: aws.Boolean(true),
+			},
+		},
+		TableName: aws.String("example_table"),
+	}
+
+	resp, err := svc.PutItem(params)
+
+	if awserr := aws.Error(err); awserr != nil {
+		fmt.Println("Error:", awserr.Code, awserr.Message)
+	} else if err != nil {
+		panic(err)
+	}
+	fmt.Println(awsutil.StringValue(resp))
+}
